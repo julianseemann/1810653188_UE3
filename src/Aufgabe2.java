d2c87b8c09ff3f1e525a3f808a2e86c2bde88250
seemanjulian <1810653188@stud.fh-kufstein.ac.at>
2018-12-02 23:00:40 +0100

    Aufgabe3

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -23,6 +23,17 @@ public class Aufgabe2
         partialString(JOptionPane.showInputDialog("Geben Sie ein Wort ein"));
         }
 
+
+        System.out.println(Taschenrechner.addition(8,4));
+        System.out.println(Taschenrechner.subtraktion(6,2));
+        System.out.println(Taschenrechner.division(4, 2));
+        System.out.println(Taschenrechner.multiplikation(5, 3));
+
+        System.out.println(Taschenrechner.addition(15, 5.5));
+        System.out.println(Taschenrechner.subtraktion(10,4.5));
+        System.out.println(Taschenrechner.division(5, 2.5));
+        System.out.println(Taschenrechner.multiplikation(3, 2));
+
     }
 
 }
---
+++ src/Taschenrechner.java
@@ -0,0 +1,39 @@
+public class Taschenrechner
+{
+
+    public static int addition(int a, int b)
+    {
+        return (a+b);
+    }
+    public static int subtraktion(int a, int b)
+    {
+        return (a-b);
+    }
+    public static int division(int a, int b)
+    {
+        return (a/b);
+    }
+    public static int multiplikation(int a, int b)
+    {
+        return (a*b);
+    }
+
+    public static double addition(double a, double b)
+    {
+        return (a+b);
+    }
+    public static double subtraktion(double a, double b)
+    {
+        return (a-b);
+    }
+    public static double division(double a, double b)
+    {
+        return (a/b);
+    }
+    public static double multiplikation(double a, double b)
+    {
+        return (a*b);
+    }
+
+
+}
